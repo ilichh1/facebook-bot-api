7791b5f0fb742be31d0c24e39d691a52dc955aa7
Ilich Arredondo <ilichh1@hotmail.com>
2020-01-29 10:59:48 -0600

    Facebook webhook

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const authRouter = require('./api/auth/index');
 const settingsRouter = require('./api/settings/index');
+const facebookRouter = require('./api/facebook/index');
 
 const DB_CONNECTION = 'mongodb+srv://admin:IF3Tn81Z23rOfXey@firstcluster-chz9d.mongodb.net/test?ssl=true&retryWrites=true&w=majority';
 mongoose.connect(DB_CONNECTION, {
@@ -29,8 +30,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/settings', settingsRouter);
 app.use('/auth', authRouter);
+app.use('/settings', settingsRouter);
+app.use('/facebook', facebookRouter);
 app.all('/', (req, res) => {
   res.send({
     message: 'My API is alive!'
