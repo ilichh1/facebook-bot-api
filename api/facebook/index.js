e0b55f9055a66387aa3e4f1b8678d77deb1bc537
Ilich Arredondo <ilichh1@hotmail.com>
2020-01-29 14:39:32 -0600

    Changes to handle facebook response

--- api/facebook/index.js
+++ api/facebook/index.js
@@ -16,23 +16,17 @@ const PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
 
 // Accepts POST requests at /webhook endpoint
 facebookRouter.post('/webhook', (req, res) => {  
-
   // Parse the request body from the POST
   let body = req.body;
-
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-
     body.entry.forEach(function(entry) {
-
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender ID: ' + sender_psid);
-
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
@@ -40,16 +34,13 @@ facebookRouter.post('/webhook', (req, res) => {
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
-      
     });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
-
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Accepts GET requests at the /webhook endpoint
